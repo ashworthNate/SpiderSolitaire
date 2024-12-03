df8efc7bf39300ff6899ecf416c3fc55a91276e0
ashworthNate <bashworth@southern.edu>
2024-12-03 14:49:41 -0500

    Update spider_solitaire.py

--- spider_solitaire.py
+++ spider_solitaire.py
@@ -3,7 +3,7 @@ Tristen, Nathan
 Spider_solitaire
 12-03-24 - Created File/Started Basic Structure
 """
-import random, enum
+import random, enum, pyQT
 
 class card:
    """Class to define cards and suits"""
@@ -18,3 +18,7 @@ class spiderSolitaire:
    """Class for the game logic of Spider Solotaire"""
    def __init__():
 
+class GUI:
+   """class to structure the GUI of the game"""
+   def __init__():
+      
